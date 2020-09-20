9872d3d2e98bca79b42277ba2c968b8f49956ff8
MasonWang025 <mason.wang0025@gmail.com>
2020-09-19 20:15:28 -0700

    script node processor implementation

--- public/js/audio/audio-handler.js
+++ public/js/audio/audio-handler.js
@@ -1,182 +1,72 @@
-let recBuffers = [[], []];
-let recLength = 0;
-let numChannels = 2;
-let listening = false;
-let timeout = null;
-let constraints = {
-  audio: true,
-};
-let failedToGetUserMedia = false;
-var globalStream = null;
+var playButton = document.querySelector("button");
 
-if (navigator.getUserMedia) {
-  navigator.getUserMedia(
-    constraints,
-    (stream) => {
-      globalStream = stream;
-      init(stream);
-    },
-    (err) => {
-      alert("Unable to access audio.\n\n" + err);
-      console.log("The following error occurred: " + err);
-      failedToGetUserMedia = true;
-    }
-  );
-} else if (navigator.mediaDevices.getUserMedia) {
-  navigator.mediaDevices
-    .getUserMedia(constraints)
-    .then((stream) => {
-      init(stream);
-    })
-    .catch((err) => {
-      alert("Unable to access audio.\n\n" + err);
-      console.log("The following error occurred: " + err);
-      failedToGetUserMedia = true;
-    });
-} else failedToGetUserMedia = true;
+// Create AudioContext and buffer source
+var audioCtx = new AudioContext();
+source = audioCtx.createBufferSource();
 
-function beginRecording() {
-  console.log("Beginning");
-  recBuffers = [[], []];
-  recLength = 0;
-  listening = true;
-  timeout = setTimeout(() => {
-    endRecording();
-  }, 5000);
-}
+// Create a ScriptProcessorNode with a bufferSize of 4096 and a single input and output channel
+var scriptNode = audioCtx.createScriptProcessor(4096, 1, 1);
+console.log(scriptNode.bufferSize);
 
-function endRecording() {
-  console.log("Done");
-  clearTimeout(timeout);
-  timeout = null;
-  let wav = exportWAV();
-  console.log(wav);
-}
+// load in an audio track via XHR and decodeAudioData
 
-function init(stream) {
-  let audioContext = new AudioContext();
-  let source = audioContext.createMediaStreamSource(stream);
-  let context = source.context;
-  let node = (
-    context.createScriptProcessor || context.createJavaScriptNode
-  ).call(context, 4096, numChannels, numChannels);
-  node.onaudioprocess = (e) => {
-    if (!listening) return;
+function getData() {
+  request = new XMLHttpRequest();
+  request.open("GET", "sampleAudio.ogg", true);
+  request.responseType = "arraybuffer";
+  request.onload = function () {
+    var audioData = request.response;
 
-    for (var i = 0; i < numChannels; i++) {
-      recBuffers[i].push([...e.inputBuffer.getChannelData(i)]);
-    }
-
-    recLength += recBuffers[0][0].length;
+    audioCtx.decodeAudioData(
+      audioData,
+      function (buffer) {
+        myBuffer = buffer;
+        source.buffer = myBuffer;
+      },
+      function (e) {
+        "Error with decoding audio data" + e.err;
+      }
+    );
   };
-  source.connect(node);
-  node.connect(context.destination);
+  request.send();
 }
 
-function mergeBuffers(buffers, len) {
-  let result = new Float32Array(len);
-  let offset = 0;
-  for (var i = 0; i < buffers.length; i++) {
-    result.set(buffers[i], offset);
-    offset += buffers[i].length;
+// Give the node a function to process audio events
+scriptNode.onaudioprocess = function (audioProcessingEvent) {
+  // The input buffer is the song we loaded earlier
+  var inputBuffer = audioProcessingEvent.inputBuffer;
+
+  // The output buffer contains the samples that will be modified and played
+  var outputBuffer = audioProcessingEvent.outputBuffer;
+
+  // Loop through the output channels (in this case there is only one)
+  for (var channel = 0; channel < outputBuffer.numberOfChannels; channel++) {
+    var inputData = inputBuffer.getChannelData(channel);
+    var outputData = outputBuffer.getChannelData(channel);
+
+    // Loop through the 4096 samples
+    for (var sample = 0; sample < inputBuffer.length; sample++) {
+      console.log(sample, outputData[sample]);
+      // make output equal to the same as the input
+      outputData[sample] = inputData[sample];
+
+      // add noise to each output sample
+      outputData[sample] += (Math.random() * 2 - 1) * 0.1;
+    }
   }
-  return result;
-}
+};
 
-function interleave(inputL, inputR) {
-  let len = inputL.length + inputR.length;
-  let result = new Float32Array(len);
+getData();
 
-  let index = 0;
-  let inputIndex = 0;
+// wire up play button
+playButton.onclick = function () {
+  source.connect(scriptNode);
+  scriptNode.connect(audioCtx.destination);
+  source.start();
+};
 
-  while (index < len) {
-    result[index++] = inputL[inputIndex];
-    result[index++] = inputR[inputIndex];
-    inputIndex++;
-  }
-
-  return result;
-}
-
-function exportWAV() {
-  console.log("exporting WAV");
-  let buffers = [];
-  for (var i = 0; i < numChannels; i++) {
-    buffers.push(mergeBuffers(recBuffers[i], recLength));
-  }
-
-  let interleaved =
-    numChannels == 2 ? interleave(buffers[0], buffers[1]) : buffers[0];
-  let dataView = encodeWAV(interleaved);
-  let blob = new Blob([dataView], { type: "audio/wav" });
-  blob.name = Math.floor(new Date().getTime() / 1000) + ".wav";
-
-  listening = false;
-
-  return blob;
-}
-
-function floatTo16BitPCM(output, offset, input) {
-  for (var i = 0; i < input.length; i++, offset += 2) {
-    var s = Math.max(-1, Math.min(1, input[i]));
-    output.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7fff, true);
-  }
-}
-
-function writeString(view, offset, string) {
-  for (var i = 0; i < string.length; i++) {
-    view.setUint8(offset + i, string.charCodeAt(i));
-  }
-}
-
-function encodeWAV(samples) {
-  var buffer = new ArrayBuffer(44 + samples.length * 2);
-  var view = new DataView(buffer);
-
-  let audioContext = new AudioContext();
-  let source = audioContext.createMediaStreamSource(globalStream);
-  let context = source.context;
-  let node = (
-    context.createScriptProcessor || context.createJavaScriptNode
-  ).call(context, 4096, numChannels, numChannels);
-
-  /* RIFF identifier */
-  writeString(view, 0, "RIFF");
-  /* file length */
-  view.setUint32(4, 36 + samples.length * 2, true);
-  /* RIFF type */
-  writeString(view, 8, "WAVE");
-  /* format chunk identifier */
-  writeString(view, 12, "fmt ");
-  /* format chunk length */
-  view.setUint32(16, 16, true);
-  /* sample format (raw) */
-  view.setUint16(20, 1, true);
-  /* channel count */
-  view.setUint16(22, numChannels, true);
-  /* sample rate */
-  view.setUint32(24, context.sampleRate, true);
-  /* byte rate (sample rate * block align) */
-  view.setUint32(28, context.sampleRate * 4, true);
-  /* block align (channel count * bytes per sample) */
-  view.setUint16(32, numChannels * 2, true);
-  /* bits per sample */
-  view.setUint16(34, 16, true);
-  /* data chunk identifier */
-  writeString(view, 36, "data");
-  /* data chunk length */
-  view.setUint32(40, samples.length * 2, true);
-
-  floatTo16BitPCM(view, 44, samples);
-
-  return view;
-}
-
-if (!failedToGetUserMedia) beginRecording();
-
-// function handleAudio(stream) {
-//   console.log("In audio handler");
-// }
-
-// module.exports = audioHandlers;
+// When the buffer source stops playing, disconnect everything
+source.onended = function () {
+  source.disconnect(scriptNode);
+  scriptNode.disconnect(audioCtx.destination);
+};
